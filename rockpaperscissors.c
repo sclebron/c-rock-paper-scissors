90456ebdceed9de91332b20e7c19cc4b0502b02d
SLeBron <sophieclebron@gmail.com>
2023-12-03 13:37:17 -0700

    completed game function and started main function

--- rockpaperscissors.c
+++ rockpaperscissors.c
@@ -5,5 +5,30 @@
 
 int game(char you, char computer)
 {
+    if (you == computer)
+        return -1;
+
+    if (you == 's' && computer == 'p')
+        return 0;
+
+    else if (you == 'p' && copmuter == 's') return 1;
     
+    if (you == 's' && computer == 'r')
+        return 1
+
+    else if (you == 'r' && copmuter == 's')
+        return 0;
+
+    if (you == 'p' && computer == 'r')
+        return 0;
+
+    else if (you == 'r' && computer == 'p')
+        return 1;
+}
+
+int main()
+{
+    int n;
+
+    char you, computer, result;
 }
