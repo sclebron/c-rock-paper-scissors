3e6d889d37677f96f3d50489b063c05b244cf9a0
SLeBron <sophieclebron@gmail.com>
2023-12-05 09:25:10 -0700

    finished first draft

--- rockpaperscissors.c
+++ rockpaperscissors.c
@@ -31,4 +31,36 @@ int main()
     int n;
 
     char you, computer, result;
+
+    srand(time(NULL));
+
+    n = rand() % 100;
+
+    if (n < 33)
+        computer = 'r';
+
+    else if (n > 33 && n < 66)
+        computer = 'p';
+
+    else 
+        computer = 'z'
+
+    printf("\n\n\n\n\t\t\t\tEnter s for STONE, p for PAPER and z for SCISSOR\n\t\t\t\t\t\t\t");
+
+    scanf("%c", &you);
+
+    result = game(you, computer);
+
+    if (result == -1)
+        prinf("\n\n\t\t\t\tGame Draw!\n");
+
+    else if (result == 1) 
+        ("\n\n\t\t\t\tGame Draw!\n");
+
+    else 
+        printf("\n\n\t\t\t\tOh! You have lost the game!\n");
+
+        printf("\t\t\t\tYOu choose : %c and Computer choose : %c\n",you, computer);
+
+    return 0;
 }
